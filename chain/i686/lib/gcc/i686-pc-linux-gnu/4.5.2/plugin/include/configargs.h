a4f6af2e4f1aae823e9ae343526eeb7bd97e9b36
Nicholas Kostelnik <nick@blackartstudios.net>
2011-01-14 09:34:06 +0000

    i586 and i686

---
+++ chain/i686/lib/gcc/i686-pc-linux-gnu/4.5.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.5.2/configure --prefix=/opt.i686 --target=i686-pc-linux-gnu --without-headers --disable-nls --enable-languages=c,c++";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentiumpro" } };
